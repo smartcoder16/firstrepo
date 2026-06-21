f89b64ee0a7bea497c810f18a21e735a974a6a21
kiran2816 <pawarkiran199728@gmail.com>
2026-06-21 17:53:59 +0530

    EMP first commit

--- github_practice/src/test/java/github_practice_demo/Begin.java
+++ github_practice/src/test/java/github_practice_demo/Begin.java
@@ -3,5 +3,6 @@ package github_practice_demo;
 public class Begin {
 	public static void main(String[] args) {
 		System.out.println("TL first change");
+		System.out.println("EMP first commit");
 	}
 }
